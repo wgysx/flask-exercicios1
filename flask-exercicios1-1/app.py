339722d4711cdfacef82a62fa2ec039d385dba97
musto <153368670+wgysx@users.noreply.github.com>
2025-11-12 15:14:06 +0000

    git add

--- flask-exercicios1-1/app.py
+++ flask-exercicios1-1/app.py
@@ -32,8 +32,8 @@ def buscar(item):
     itens = ['maçã', 'banana', 'laranja', 'uva']
     for i in itens:
         if i == item:
-            return f'Item "{item}" encontrado.'
-    return f'Item "{item}" não encontrado.'
+            return f'Item \"{item}\" encontrado.'
+    return f'Item \"{item}\" não encontrado.'
 
 if __name__ == '__main__':
     app.run(debug=True)
