af8f2b5e688f6aff7c799bfd81cc8959e229bf2a
musto <153368670+wgysx@users.noreply.github.com>
2025-11-12 14:40:48 +0000

    feat: adicionar index e rota /versao

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, Flask !!</h1>'
+
+@app.route('/versao')
+def versao():
+    versao = "1.1.0"
+    return f"App v{versao}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
